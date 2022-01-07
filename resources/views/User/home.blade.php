703c2a1f976a9130e85d27b2ef4b031ba234f4ed
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-01-07 20:21:52 +0200

    Update home.blade.php

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -14,7 +14,7 @@
     <title>Sixteen Clothing HTML Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!--
 
 TemplateMo 546 Sixteen Clothing
@@ -24,9 +24,9 @@ https://templatemo.com/tm-546-sixteen-clothing
 -->
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome.css">
+    <link rel="stylesheet" href="./assets/css/templatemo-sixteen.css">
+    <link rel="stylesheet" href="./assets/css/owl.css">
 
   </head>
 
