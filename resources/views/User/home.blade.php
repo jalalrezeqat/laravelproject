d933f3216c0a4705567c5554324df8b7e7496f5d
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-01-07 19:50:59 +0200

    Update home.blade.php

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -3,7 +3,12 @@
 
   <head>
 
-   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
 
     <title>Sixteen Clothing HTML Template</title>
