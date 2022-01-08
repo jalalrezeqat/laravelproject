6e1408c2ab827e859c5200b483ed410757e2ca14
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-01-08 14:17:32 +0200

    Update home.blade.php

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -25,15 +25,22 @@ https://templatemo.com/tm-546-sixteen-clothing
 -->
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="/assets/css/fontawesome.css">
-    <link rel="stylesheet" href="/assets/css/templatemo-sixteen.css">
-    <link rel="stylesheet" href="/assets/css/owl.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="../assets/css/templatemo-sixteen.css">
+    <link rel="stylesheet" href="../assets/css/owl.css">
+
 
   </head>
   <body>
 
     <!-- ***** Preloader Start ***** -->
-    
+     <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
     <!-- ***** Preloader End ***** -->
 
     <!-- Header -->
